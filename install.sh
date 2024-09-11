c97ebd2f337cd62c7a8266473e78de50f2c27766
Rajat Gupta <142142387+RajatGupta0607@users.noreply.github.com>
2024-09-11 23:02:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,14 +17,14 @@ else
     echo "1 -> python3-virtualenv AND python3-venv"
     echo "2 -> PyGObject" 
     echo "3 -> PyCairo" 
-    echo "4 -> Expert" 
+    echo "4 -> Expect" 
     echo "----------Creating Folder for Open Freeze Center----------"
     cd ~/Desktop
     mkdir OFC
     echo "----------Installing python3-virtualenv AND python3-venv and other dependencies----------"
-    sudo apt update
-    sudo apt upgrade
-    sudo apt install python3-virtualenv python3-venv libgirepository1.0-dev libcairo2-dev
+    sudo dnf update -y
+    sudo dnf upgrade -y
+    sudo dnf install -y python3-virtualenv python3-venv gobject-introspection-devel cairo-devel expect
     echo "----------Creating Virtual Environment for Open Freeze Center----------"
     python3 -m venv ~/Desktop/OFC
     echo "----------Virtual Environment for Open Freeze Center created----------"
@@ -32,15 +32,15 @@ else
     ~/Desktop/OFC/bin/pip3 install PyGObject
     echo "----------Installing PyCairo----------"
     ~/Desktop/OFC/bin/pip3 install pycairo
-    echo "----------Installing Expert----------"
-    sudo apt-get install expect
+    echo "----------Installing Expect----------"
+    sudo dnf install -y expect
     echo "----------Moving files to virtual environment----------"
-    cp -i ~/Downloads/OpenFreezeCenter-5/install.sh  ~/Desktop/OFC
-    cp -i ~/Downloads/OpenFreezeCenter-5/file_1.sh  ~/Desktop/OFC
-    cp -i ~/Downloads/OpenFreezeCenter-5/file_2.sh  ~/Desktop/OFC
-    cp -i ~/Downloads/OpenFreezeCenter-5/OFC.py  ~/Desktop/OFC
-    cp -i ~/Downloads/OpenFreezeCenter-5/README.md  ~/Desktop/OFC
-    cp -i ~/Downloads/OpenFreezeCenter-5/LICENSE  ~/Desktop/OFC
+    cp -i ~/Downloads/OpenFreezeCenter-5/install.sh ~/Desktop/OFC
+    cp -i ~/Downloads/OpenFreezeCenter-5/file_1.sh ~/Desktop/OFC
+    cp -i ~/Downloads/OpenFreezeCenter-5/file_2.sh ~/Desktop/OFC
+    cp -i ~/Downloads/OpenFreezeCenter-5/OFC.py ~/Desktop/OFC
+    cp -i ~/Downloads/OpenFreezeCenter-5/README.md ~/Desktop/OFC
+    cp -i ~/Downloads/OpenFreezeCenter-5/LICENSE ~/Desktop/OFC
     FLAG_3=1
 fi
 
@@ -69,7 +69,7 @@ then
     fi
 else
     echo "----------Prepairing system for EC read/write----------"
-    mkdir /etc/modprobe.d
+    sudo mkdir /etc/modprobe.d
     sudo touch /etc/modprobe.d/ec_sys.conf
     cd ~/Desktop/OFC/
     sudo ./file_1.sh
@@ -97,7 +97,7 @@ then
     fi
 else
     echo "----------Prepairing system for EC read/write----------"
-    mkdir /etc/modules-load.d
+    sudo mkdir /etc/modules-load.d
     sudo touch /etc/modules-load.d/ec_sys.conf
     cd ~/Desktop/OFC/
     sudo ./file_2.sh
@@ -108,7 +108,7 @@ if [ "$FLAG_1" -eq 1 ] && [ "$FLAG_2" -eq 1 ];
 then
     echo "----------EC read/write is enabled----------"
 else
-    echo "----------EC read/write is can not be enabled----------"
+    echo "----------EC read/write cannot be enabled----------"
 fi
 
 if [ "$FLAG_3" -eq 1 ];
@@ -116,9 +116,9 @@ then
     if test -f ~/Desktop/OFC/config.py;
     then
         echo "----------Running Software----------"
-        sudo nohup ~/Desktop/OFC/bin/python3 ~/Desktop/OFC/OFC.py
+        sudo nohup ~/Desktop/OFC/bin/python3 ~/Desktop/OFC/OFC.py &
     else
         echo "----------Running Software----------"
-        sudo nohup ~/Desktop/OFC/bin/python3 ~/Desktop/OFC/OFC.py
+        sudo nohup ~/Desktop/OFC/bin/python3 ~/Desktop/OFC/OFC.py &
     fi
 fi
